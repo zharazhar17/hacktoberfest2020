27c7581ae839011f96e73b6b748c1c1083445c31
Ahan Pahlevi <50755407+AhanDotID@users.noreply.github.com>
2020-10-16 21:19:50 +0700

    Create check_internet_wget.sh

---
+++ FILE/check_internet_wget.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+WGET="/usr/bin/wget"
+
+$WGET -q --tries=20 --timeout=10 http://www.google.com -O /tmp/google.idx &> /dev/null
+if [ ! -s /tmp/google.idx ]
+then
+    echo "Not Connected..!"
+else
+    echo "Connected..!"
+fi
