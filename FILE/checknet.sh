7fbeead2d03024fe49ff33235b95aa9b86079ec1
hamran2631 <72916089+hamran2631@users.noreply.github.com>
2020-10-16 20:36:22 +0700

    Create checknet.sh

---
+++ FILE/checknet.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+# Test for network conection
+for interface in $(ls /sys/class/net/ | grep -v lo);
+do
+  if [[ $(cat /sys/class/net/$interface/carrier) = 1 ]]; then OnLine=1; fi
+done
+if ! [ $OnLine ]; then echo "Not Online" > /dev/stderr; exit; fi
